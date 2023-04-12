f8056c47024a59bd3a070141fb4a3b3ef2519962
juhohamalainen <59827017+juhohamalainen@users.noreply.github.com>
2023-04-12 17:01:05 +0300

    Navigation with routing
    -Dashboard view
    -Navigation between the Heroes and Dashboard views
    -Clicking a hero name in either view, navigate to a detail view of the selected hero
     -"Deep link" opens the detail view for a particular hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
-//Component class code, written in TypeScript.
-//Import the Component symbol from the Angular core library
 import { Component } from '@angular/core';
 
-//Decorator function that specifies the Angular metadata for the component
 @Component({
-  //The component's CSS element selector.
   selector: 'app-root',
-  //The location of the component's template file.
   templateUrl: './app.component.html',
-  //The location of the component's private CSS styles.
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
